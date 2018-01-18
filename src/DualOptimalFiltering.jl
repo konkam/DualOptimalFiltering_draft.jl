8dd5a6e19c85aef2316feaf96a2873f1062ea8fc
konkam <guillaume.konkamking.work@gmail.com>
2018-01-18 19:15:44 +0100

    wrote the 1D-CIR filtering

--- src/DualOptimalFiltering.jl
+++ src/DualOptimalFiltering.jl
@@ -2,4 +2,7 @@ module DualOptimalFiltering
 
 # package code goes here
 
+include("CIRfiltering.jl")
+include("CommonUtilityFunctions.jl")
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
 using DualOptimalFiltering
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+using Distributions
+# Run tests
+#@test 1 == 2
+
+tic()
+println("Testing common utility functions")
+@time include("tests_CommonUtilityFunctions.jl")
+
+println("Testing CIR filtering")
+@time include("tests_CIRfiltering.jl")
+
+toc()
