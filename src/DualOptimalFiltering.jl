70d72b4a0e1a1be6aae3d768e127b69426197656
konkam <guillaume.konkamking.work@gmail.com>
2018-01-18 19:22:32 +0100

    fixed wrong command

--- src/DualOptimalFiltering.jl
+++ src/DualOptimalFiltering.jl
@@ -2,7 +2,7 @@ module DualOptimalFiltering
 
 # package code goes here
 
-@export generate_CIR_trajectory, filter_CIR
+export generate_CIR_trajectory, filter_CIR
 
 include("CIRfiltering.jl")
 include("CommonUtilityFunctions.jl")
