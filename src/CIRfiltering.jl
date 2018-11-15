42ec4fa5ea0c6c0aab181db63a269c14301d0293
konkam <guillaume.konkamking.work@gmail.com>
2018-11-15 16:05:51 +0100

    simulation from CIR was missing for simulating new data

--- src/CIRfiltering.jl
+++ src/CIRfiltering.jl
@@ -220,6 +220,17 @@ end
 #     xs = Float64[rand(Gamma(k+δ/2, 1/rate)) for k in ks]
 # end
 
+function rCIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ)
+    β = γ/σ^2*exp(2*γ*Dt)/(exp(2*γ*Dt)-1)
+    if n == 1
+        ks = rand(Poisson(γ/σ^2*x0/(exp(2*γ*Dt)-1)))
+        return rand(Gamma(ks+δ/2, 1/β))
+    else
+        ks = rand(Poisson(γ/σ^2*x0/(exp(2*γ*Dt)-1)), n)
+        return rand.(Gamma.(ks+δ/2, 1/β))
+    end
+end
+
 transition_CIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ) = rCIR(n::Integer, Dt::Real, x0::Real, δ, γ, σ)
 
 function rec_transition_CIR(Dts, x, δ, γ, σ)
