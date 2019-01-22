3c232a0852f8ebab3ce0703a68bee7081e2ecabe
konkam <guillaume.konkamking.work@gmail.com>
2019-01-22 16:49:06 +0100

    approx filtering functions with precomputing

--- src/WFfiltering_adaptive_precomputation_approx.jl
+++ src/WFfiltering_adaptive_precomputation_approx.jl
@@ -9,3 +9,29 @@ function filter_WF_adaptive_precomputation_keep_fixed_number(α, data, fixed_num
     filter_WF_adaptive_precomputation_ar(α, data, prune_keeping_fixed_number; silence = silence)
 
 end
+
+function filter_WF_adaptive_precomputation_keep_above_threshold(α, data, ε::Float64; silence = false)
+    # println("filter_WF_mem2")
+
+    function prune_keeping_above_threshold(Λ_of_t, wms_of_t)
+        Λ_of_t_kept, wms_of_t_kept = keep_above_threshold(Λ_of_t, wms_of_t, ε)
+        return Λ_of_t_kept, normalise(wms_of_t_kept)
+    end
+
+    filter_WF_adaptive_precomputation_ar(α, data, prune_keeping_above_threshold; silence = silence)
+
+end
+
+
+function filter_WF_adaptive_precomputation_keep_fixed_fraction(α, data, fraction::Float64; silence = false)
+    # println("filter_WF_mem2")
+
+    function prune_keeping_fixed_fraction(Λ_of_t, wms_of_t)
+        Λ_of_t_kept, wms_of_t_kept = keep_fixed_fraction(Λ_of_t, wms_of_t, fraction)
+        return Λ_of_t_kept, normalise(wms_of_t_kept)
+    end
+
+
+    filter_WF_adaptive_precomputation_ar(α, data, prune_keeping_fixed_fraction; silence = silence)
+
+end
