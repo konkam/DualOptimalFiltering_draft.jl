43ae226224c3610b5ea31cd3fa984eb2a39b1e61
konkam <guillaume.konkamking.work@gmail.com>
2019-01-22 16:30:29 +0100

    moving include to top in a hope to avoid having to change the functions

--- src/DualOptimalFiltering.jl
+++ src/DualOptimalFiltering.jl
@@ -3,9 +3,9 @@ module DualOptimalFiltering
 # package code goes here
 using Printf
 
-# export generate_CIR_trajectory, filter_CIR, filter_WF, bind_rows, Hellinger_dist_1D, dirichletkernel
+export generate_CIR_trajectory, filter_CIR, filter_WF, bind_rows, Hellinger_dist_1D, dirichletkernel
 
-
+include("CommonUtilityFunctions.jl")
 include("CIRfiltering.jl")
 include("CIRfiltering_approximate.jl")
 include("WFfiltering.jl")
@@ -14,14 +14,15 @@ include("WFfiltering_precomputed_approximate.jl")
 include("WFfiltering_precompute_Nemo_arbitrary_precision.jl")
 include("WFfiltering_precompute_ar.jl")
 include("WFfiltering_adaptive_precomputation.jl")
+include("WFfiltering_adaptive_precomputation_approx.jl")
 include("WF_likelihood.jl")
-include("CommonUtilityFunctions.jl")
 include("pruning_functions.jl")
 include("finite_size_wright_fisher_simulation.jl")
+include("post_process_Dirichlet_mixture_posterior.jl")
 # include("plot_data_and_posterior_Dirichlet_mixture.jl")
 # include("plot_data_and_posterior_Gamma_mixture.jl")
 # include("statistical_distances_on_the_simplex.jl")
-# include("statistical_distances_on_Rplus.jl")
+include("statistical_distances_on_Rplus.jl")
 include("dirichlet_kde.jl")
 # # include("generic_particle_filter_algorithm.jl")
 include("CIR_likelihood.jl")
