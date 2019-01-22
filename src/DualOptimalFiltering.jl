ae5c01188ffa444a510427a1a002f27cbb8cb72c
konkam <guillaume.konkamking.work@gmail.com>
2019-01-22 13:00:59 +0100

    RCall does not work on Julia 1.1

--- src/DualOptimalFiltering.jl
+++ src/DualOptimalFiltering.jl
@@ -3,7 +3,7 @@ module DualOptimalFiltering
 # package code goes here
 using Printf
 
-export generate_CIR_trajectory, filter_CIR, filter_WF, bind_rows, Hellinger_dist_1D, dirichletkernel
+# export generate_CIR_trajectory, filter_CIR, filter_WF, bind_rows, Hellinger_dist_1D, dirichletkernel
 
 
 include("CIRfiltering.jl")
@@ -11,8 +11,6 @@ include("CIRfiltering_approximate.jl")
 include("WFfiltering.jl")
 include("WFfiltering_precompute.jl")
 include("WFfiltering_precomputed_approximate.jl")
-# include("WFfiltering_precompute_preallocate.jl")
-# include("WFfiltering_precompute_preallocate_n.jl")
 include("WFfiltering_precompute_Nemo_arbitrary_precision.jl")
 include("WFfiltering_precompute_ar.jl")
 include("WFfiltering_adaptive_precomputation.jl")
@@ -20,12 +18,12 @@ include("WF_likelihood.jl")
 include("CommonUtilityFunctions.jl")
 include("pruning_functions.jl")
 include("finite_size_wright_fisher_simulation.jl")
-include("plot_data_and_posterior_Dirichlet_mixture.jl")
-include("plot_data_and_posterior_Gamma_mixture.jl")
-include("statistical_distances_on_the_simplex.jl")
-include("statistical_distances_on_Rplus.jl")
+# include("plot_data_and_posterior_Dirichlet_mixture.jl")
+# include("plot_data_and_posterior_Gamma_mixture.jl")
+# include("statistical_distances_on_the_simplex.jl")
+# include("statistical_distances_on_Rplus.jl")
 include("dirichlet_kde.jl")
-# include("generic_particle_filter_algorithm.jl")
+# # include("generic_particle_filter_algorithm.jl")
 include("CIR_likelihood.jl")
 include("WF_particle_filter.jl")
 include("exact_L2_distances.jl")
