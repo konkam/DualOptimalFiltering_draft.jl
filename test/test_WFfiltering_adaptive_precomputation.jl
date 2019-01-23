082418317ba82b343e7e4f344a3cadfadf6e1ff4
konkam <guillaume.konkamking.work@gmail.com>
2019-01-23 16:30:56 +0100

    more tests for adaptive precomputation

--- test/test_WFfiltering_adaptive_precomputation.jl
+++ test/test_WFfiltering_adaptive_precomputation.jl
@@ -41,3 +41,18 @@ end;
         end
     end
 end;
+
+@testset "test adaptive precomputation approx filtering functions" begin
+
+    Random.seed!(4)
+    wfchain = rand(Dirichlet(4,0.3)) |> z-> DualOptimalFiltering.wright_fisher_PD1(z, 1.5, 50, 3)[:,2:end]*10 |> x -> round.(x) |> x -> Int64.(x)
+    data = Dict(zip(range(0, stop = 5, length = size(wfchain,2)), [collect(collect(wfchain[:,t:t]')) for t in 1:size(wfchain,2)]))
+    # Λ_of_t, wms_of_t = DualOptimalFiltering.filter_WF_mem2(ones(4), data)
+    α = ones(4)
+
+    @test_nowarn DualOptimalFiltering.filter_WF_adaptive_precomputation_keep_fixed_number(α, data, 5; silence = false)
+
+    @test_nowarn DualOptimalFiltering.filter_WF_adaptive_precomputation_keep_above_threshold(α, data, 0.01; silence = false)
+
+    @test_nowarn DualOptimalFiltering.filter_WF_adaptive_precomputation_keep_fixed_fraction(α, data, 0.95; silence = false)
+end;
