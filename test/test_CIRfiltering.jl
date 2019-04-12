5fb761611ba4b172528b35705d9357eade16ef4e
konkam <guillaume.konkamking.work@gmail.com>
2019-04-12 14:59:23 +0200

    test for pruning on log weights

--- test/test_CIRfiltering.jl
+++ test/test_CIRfiltering.jl
@@ -73,4 +73,13 @@ end;
     Λ_of_t, wms_of_t = DualOptimalFiltering.filter_CIR_fixed_fraction(3., 0.5, 1.,1., data, .99)
     @test length(keys(Λ_of_t)) == 20
     @test length(keys(wms_of_t)) == 20
+
+
+    fixed_number = 10
+    function prune_keeping_fixed_number(Λ_of_t, wms_of_t)
+        Λ_of_t_kept, wms_of_t_kept = DualOptimalFiltering.keep_fixed_number_of_weights(Λ_of_t, wms_of_t, fixed_number)
+        return Λ_of_t_kept, DualOptimalFiltering.normalise(wms_of_t_kept)
+    end
+
+    Λ_of_t, wms_of_t = DualOptimalFiltering.filter_CIR_pruning_logweights(3., 0.5, 1.,1., data, prune_keeping_fixed_number; silence = false)
 end;
