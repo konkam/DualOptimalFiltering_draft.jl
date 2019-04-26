60d83fca69a1e78434af8795cd5848cbced67f88
konkam <guillaume.konkamking.work@gmail.com>
2019-04-26 18:32:58 +0200

    needed

--- src/WF_smoothing_approximate.jl
+++ src/WF_smoothing_approximate.jl
@@ -23,7 +23,7 @@ function smooth_CIR_keep_fixed_number(δ, γ, σ, λ, data, fixed_number::Int64;
     Λ_of_t, logwms_of_t, θ_of_t = filter_CIR_pruning_logweights(δ, γ, σ, λ, data, prune_keeping_fixed_number; silence = silence)
     Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t = log_cost_to_go_CIR_keep_fixed_number(δ, γ, σ, λ, data, fixed_number::Int64; silence = silence)
 
-    Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth = merge_filtering_and_cost_to_go_logweights(Λ_of_t, logwms_of_t, θ_of_t, Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t, data, α, β)
+    Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth = merge_filtering_and_cost_to_go_logweights_CIR(Λ_of_t, logwms_of_t, θ_of_t, Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t, data, α, β)
 
     return Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth
 end
@@ -62,7 +62,7 @@ function smooth_CIR_keep_above_threshold(δ, γ, σ, λ, data, ε_filter::Real, 
 
     Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t = log_cost_to_go_CIR_keep_above_threshold(δ, γ, σ, λ, data, logε_cost_to_go; silence = silence)
 
-    Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth = merge_filtering_and_cost_to_go_logweights(Λ_of_t, logwms_of_t, θ_of_t, Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t, data, α, β)
+    Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth = merge_filtering_and_cost_to_go_logweights_CIR(Λ_of_t, logwms_of_t, θ_of_t, Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t, data, α, β)
 
     return Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth
 end
@@ -98,7 +98,7 @@ function smooth_CIR_keep_fixed_fraction(δ, γ, σ, λ, data, fraction::Float64;
 
     Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t = log_cost_to_go_CIR_keep_fixed_fraction(δ, γ, σ, λ, data, fraction::Float64; silence = silence)
 
-    Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth = merge_filtering_and_cost_to_go_logweights(Λ_of_t, logwms_of_t, θ_of_t, Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t, data, α, β)
+    Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth = merge_filtering_and_cost_to_go_logweights_CIR(Λ_of_t, logwms_of_t, θ_of_t, Λ_tilde_prime_of_t, logwms_tilde_of_t, θ_tilde_prime_of_t, data, α, β)
 
     return Λ_of_t_smooth, wms_of_t_smooth, θ_of_t_smooth
 end
