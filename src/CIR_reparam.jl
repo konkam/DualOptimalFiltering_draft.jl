11d7203ac4d9a76337294fa82572c1ddb47b8e30
konkam <guillaume.konkamking.work@gmail.com>
2019-11-11 12:09:02 +0100

    implemented reparametrisation of the Cox Ingersoll Ross model

--- src/CIR_reparam.jl
+++ src/CIR_reparam.jl
@@ -15,3 +15,97 @@ function inverse_reparam_CIR(a, b, σ_prime)
     σ = σ_prime/2
     return δ, γ, σ
 end
+
+function reparam_joint_loglikelihood_CIR(data, trajectory, times, a, b, σ_prime, λ)
+    δ, γ, σ = inverse_reparam_CIR(a, b, σ_prime)
+    return joint_loglikelihood_CIR(data, trajectory, times, δ, γ, σ, λ)
+end
+
+
+function joint_sampler_CIR_reparam_pruning_precompute(data, λ, prior_logpdf, niter, do_the_pruning::Function; final_chain_length = 1000, silence = false, jump_sizes = (0.5, 0.5, 0.5), θ_init = (1.,1.,1.))
+
+    trajectory_chain = Array{Float64,2}(undef, niter, length(data |> keys))
+    parameter_chain = Array{Float64,2}(undef, niter, 3)
+    times = data |> keys |> collect |> sort
+
+    function unnormalised_logposterior(a, b, σ_prime, trajectory)::Float64
+        prior_contribution = prior_logpdf(a, b, σ_prime)
+        if isinf(prior_contribution) #Avoids likelihood computation in this case
+            return prior_contribution
+        else
+            return prior_contribution + reparam_joint_loglikelihood_CIR(data, trajectory, times, a, b, σ_prime, λ)::Float64
+        end
+    end
+
+    θ_it = θ_init
+
+    Jtsym_rand(θ) = rand.(Normal.(0, jump_sizes)) .+ θ
+
+    for it in 1:niter
+
+        print_every = max(1, floor(niter/20))
+
+        if !silence && mod(it, print_every) == 0
+            @info "$it iterations out of $niter"
+        end
+
+        # Sample trajectory
+
+        #Not using the logfunction because not implemented yet
+        θ_it_δγ_param = inverse_reparam_CIR(θ_it...)
+        Λ_of_t, wms_of_t, θ_of_t = DualOptimalFiltering.filter_CIR_pruning(θ_it_δγ_param[1], θ_it_δγ_param[2], θ_it_δγ_param[3], λ, data, do_the_pruning, silence = true)
+        Λ_of_t_pruned, wms_of_t_pruned =  prune_all_dicts(Λ_of_t, wms_of_t, do_the_pruning)
+        # logwms_of_t = log.(wms_of_t)
+        # logwms_pred_of_t = log.(wms_pred_of_t)
+        # 1, 1, 1 are dummy arguments
+        X_it = sample_1_trajectory_from_joint_smoothing_CIR_precompute(θ_it_δγ_param[1], θ_it_δγ_param[2], θ_it_δγ_param[3], Λ_of_t_pruned, wms_of_t_pruned, θ_of_t, 1, 1, 1, data)
+
+        @debug "iteration $it"
+        @debug "trajectory" X_it
+
+        # Metropolis-Hastings step
+
+        θ_it = draw_next_sample(θ_it, Jtsym_rand, st -> unnormalised_logposterior(st[1], st[2], st[3], X_it))
+
+        # Record iteration
+
+        trajectory_chain[it,:] = X_it
+        parameter_chain[it,:] = collect(θ_it)
+
+    end
+
+    return parameter_chain, trajectory_chain
+end
+
+function joint_sampler_CIR_reparam_keep_fixed_number_precompute(data, λ, prior_logpdf, niter, fixed_number::Int64; final_chain_length = 1000, silence = false, jump_sizes = (0.5, 0.5, 0.5), θ_init = (1.,1.,1.))
+    # println("filter_WF_mem2")
+
+    function prune_keeping_fixed_number(Λ_of_t, wms_of_t)
+        Λ_of_t_kept, wms_of_t_kept = keep_fixed_number_of_weights(Λ_of_t, wms_of_t, fixed_number)
+        return Λ_of_t_kept, normalise(wms_of_t_kept)
+    end
+
+    joint_sampler_CIR_reparam_pruning_precompute(data, λ, prior_logpdf, niter, prune_keeping_fixed_number; final_chain_length = 1000, silence = silence, jump_sizes = jump_sizes, θ_init = θ_init)
+
+end
+
+
+# function joint_RAM_sampler_CIR_reparam_pruning_precompute(data, λ, prior_logpdf, θ_init, niter, do_the_pruning::Function; final_chain_length = 1000, silence = false, jump_size = 0.5)
+#     trajectory_chain = Array{Float64,2}(undef, niter, length(data |> keys))
+#     # parameter_chain = Array{Float64,2}(undef, niter, 3)
+#     times = data |> keys |> collect |> sort
+#
+#     function unnormalised_logposterior(δ, γ, σ, trajectory)::Float64
+#         prior_contribution = prior_logpdf(δ, γ, σ)
+#         if isinf(prior_contribution) #Avoids likelihood computation in this case
+#             return prior_contribution
+#         else
+#             return prior_contribution + joint_loglikelihood_CIR(data, trajectory, times, δ, γ, σ, λ)::Float64
+#         end
+#     end
+#
+#     it = 1
+#
+#     function MH_plus_Gibbs_step()
+#
+# end
--- test/test_CIR_reparam.jl
+++ test/test_CIR_reparam.jl
@@ -6,4 +6,24 @@
     for pidx in eachindex([δ, γ, σ])
         @test [δ, γ, σ][pidx] ≈ DualOptimalFiltering.reparam_CIR(DualOptimalFiltering.inverse_reparam_CIR(δ, γ, σ)...)[pidx]
     end
+
+    Random.seed!(0)
+    times_sim = range(0, stop = 20, length = 20)
+    X = DualOptimalFiltering.generate_CIR_trajectory(times_sim, 3, 3., 0.5, 1);
+    Y = map(λ -> rand(Poisson(λ),1), X);
+    data = Dict(zip(times_sim, Y))
+    δ, γ, σ, λ = 3., 0.5, 1., 1.
+    a, b, σ_prime = DualOptimalFiltering.reparam_CIR(δ, γ, σ)
+    Λ_of_t, logwms_of_t, θ_of_t, Λ_pred_of_t, logwms_pred_of_t, θ_pred_of_t = DualOptimalFiltering.filter_predict_CIR_logweights(δ, γ, σ, λ, data);
+
+    times = Λ_of_t |> keys |> collect |> sort
+
+    @test DualOptimalFiltering.reparam_joint_loglikelihood_CIR(data, X, times, a, b, σ_prime, λ) == DualOptimalFiltering.joint_loglikelihood_CIR(data, X, times, δ, γ, σ, λ)
+
+    prior_a = Truncated(Normal(5, 4), 0, Inf)
+    prior_b = Truncated(Normal(5, 4), 0, Inf)
+    prior_σ_prime = Truncated(Normal(5, 4), 0, Inf)
+    prior_logpdf(ai, bi, σ_primei) = logpdf(prior_a, ai) + logpdf(prior_b, bi) + logpdf(prior_σ_prime, σ_primei)
+
+    @test_nowarn DualOptimalFiltering.joint_sampler_CIR_reparam_keep_fixed_number_precompute(data, λ, prior_logpdf, 10, 2; final_chain_length = 10)
 end;
